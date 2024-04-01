7d86fb5ec362c4a7ae6fb2864022f6e95d5ad5b2
jayapriya1098V <jayapriya1098@gmail.com>
2024-04-02 00:13:54 +0530

    Upload code to git repo

---
+++ src/org/sample/Sample.java
@@ -0,0 +1,28 @@
+package org.sample;
+
+	public class Sample {
+		
+		public Sample() {
+			this("Jaya");
+			System.out.println("Constructor");
+			
+		}
+		public Sample(String name) {
+			
+			this(10,"Chennai");
+			System.out.println("Emp name: "+name);
+		}
+		
+		public Sample(int id, String location) {
+			this(55.5);
+			System.out.println("Emp id: "+id);
+			System.out.println("Loation: "+location);
+			
+		}
+		public Sample(double no){
+			System.out.println("no: "+no);
+		}
+		public static void main(String[] args) {
+			Sample s = new Sample();
+		}
+		}
